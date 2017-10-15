b14c6cd66d5e16a12c523ec949bd8e4a57eb12b4
Erik Edin <erikedin@users.noreply.github.com>
2017-10-15 19:23:29 +0200

    plumber: add encoding test for int fields

--- plumber/encoding_test.go
+++ plumber/encoding_test.go
@@ -12,6 +12,10 @@ type StringRecord struct {
 	Field2 string `avro:"field2"`
 }
 
+type IntRecord struct {
+	Field1 int32 `avro:"field1"`
+}
+
 func TestEncodeDecode_AllStringFields_DecodedMatchesOriginal(t *testing.T) {
 	codec, err := goavro.NewCodec(`
 		{
@@ -35,6 +39,35 @@ func TestEncodeDecode_AllStringFields_DecodedMatchesOriginal(t *testing.T) {
 	encoded, _ := encoding.Encode(original)
 
 	var decoded StringRecord
+	encoding.Decode(encoded, &decoded)
+
+	if original != decoded {
+		t.Errorf("Original %v did not match decodede %v", original, decoded)
+	}
+}
+
+func TestEncodeDecode_IntField_DecodedMatchesOriginal(t *testing.T) {
+	codec, err := goavro.NewCodec(`
+		{
+			"namespace": "plumber",
+			"type": "record",
+			"name": "IntTest",
+			"fields": [
+			  {"name": "field1",  "type": "int"}
+			]
+		}`)
+	if err != nil {
+		t.Errorf("Codec is nil: %v", err)
+		return
+	}
+
+	encoding := RecordEncoding{codec, reflect.TypeOf(IntRecord{})}
+
+	original := IntRecord{42}
+
+	encoded, _ := encoding.Encode(original)
+
+	var decoded IntRecord
 	encoding.Decode(encoded, &decoded)
 
 	if original != decoded {
