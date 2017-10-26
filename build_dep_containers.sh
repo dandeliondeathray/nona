74a3cc92982941fa658bfd0145fdfae6de79aa93
Erik Edin <erikedin@users.noreply.github.com>
2017-10-26 12:30:15 +0200

    build librdkafka container

---
+++ build_dep_containers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION=$1
+if [ "${VERSION}" == "" ]
+then
+	VERSION="0.11.1"
+fi
+
+sudo docker build --build-arg VERSION=${VERSION} -t erikedin/librdkafka:${VERSION}  containers/librdkafka
