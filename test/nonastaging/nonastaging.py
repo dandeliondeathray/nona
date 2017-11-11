e233a87541bd975a6e70eb79d0f6b6e895c1a8f7
Erik Edin <erikedin@users.noreply.github.com>
2017-11-11 20:59:52 +0100

    untested implementation of nonastaging

--- test/nonastaging/nonastaging.py
+++ test/nonastaging/nonastaging.py
@@ -5,21 +5,74 @@ acceptance testing of the Nona system deployed in a staging environment.
 
 import asyncio
 import os
+import threading
+import json
 from nonainterface import NonaInterface, AvroSchemas
+import websockets
 
 
 class Nonastaging:
     """Nonastaging implements the entire micro service."""
-    def __init__(self, bootstrap_servers, schema_path):
+    def __init__(self, loop, bootstrap_servers, schema_path):
+        self.loop = loop
         schemas = AvroSchemas(schema_path)
         self._nona = NonaInterface("staging", bootstrap_servers, schemas)
+        self._clients = []
+        self._chat_event_reader = None
+
+    def read_chat_events(self):
+        while True:
+            chat_event = self._nona.chat_events.get(block=True)
+            if chat_event is None:
+                break
+            self.loop.call_soon_threadsafe(self.send_chat_event, chat_event)
+
+    def stop_chat_event_reader(self):
+        self._nona.chat_events.put(None)
+        self._chat_event_reader.join()
+
+    @asyncio.coroutine
+    def send_chat_event(self, chat_event):
+        clients = self._clients[:]
+        chat_event_json = {'user_id': chat_event.user_id,
+                           'team': chat_event.team,
+                           'text': chat_event.text}
+        for client in clients:
+            yield from client.send(json.dumps(chat_event_json))
+
+    @asyncio.coroutine
+    def handle_command(self, msg):
+        command = json.loads(msg)
+        if command['name'] == 'user_requests_puzzle':
+            user_id = command['user_id']
+            self._nona.user_requests_puzzle(user_id)
+        else:
+            print("Invalid command:", command)
+
+    @asyncio.coroutine
+    def read_commands(self, websocket):
+        while True:
+            msg = yield from websocket.recv()
+            self.loop.call_soon(self.handle_command, msg)
+
+    @asyncio.coroutine
+    def add_client(self, websocket, _path):
+        print("New connection")
+        self._clients.append(websocket)
+        self.loop.call_soon(self.read_commands, websocket)
 
     def run_forever(self):
         """Start the event loop and only return when it is stopped."""
-        loop = asyncio.get_event_loop()
-        loop.run_forever()
+        self._nona.start()
+        self._chat_event_reader = threading.Thread(target=self.read_chat_events)
+        self._chat_event_reader.start()
 
+        start_server = websockets.serve(self.add_client, '0.0.0.0', 8765)
+        self.loop.run_until_complete(start_server)
+        self.loop.run_forever()
 
 if __name__ == "__main__":
-    nonastaging = Nonastaging(os.environ['KAFKA_BROKERS'], os.environ['SCHEMA_PATH'])
+    myloop = asyncio.get_event_loop()
+    nonastaging = Nonastaging(myloop, os.environ['KAFKA_BROKERS'], os.environ['SCHEMA_PATH'])
     nonastaging.run_forever()
+    nonastaging.stop_chat_event_reader()
